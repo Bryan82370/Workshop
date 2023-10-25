e2eb615ebc385f85d5759cc3a06b48b79fa60187
Bryan82370 <hellerbryan.pro@gmail.com>
2023-10-25 10:32:59 +0200

    route + page création

--- workshop/app/urls.py
+++ workshop/app/urls.py
@@ -4,4 +4,14 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about_us', views.about_us, name='about_us'),
+    path('dashboard', views.dashboard, name='dashboard'),
+    path('my_missions', views.my_missions, name='my_missions'),
+    path('mission_available', views.mission_available, name='mission_available'),
+    path('map', views.map, name='map'),
+    path('settings', views.settings, name='settings'),
+    path('partenaire', views.partenaire, name='partenaire'),
+    path('login', views.login, name='login'),
+    path('registration', views.registration, name='registration'),
+    path('informations', views.informations, name='informations'),
 ]
--- workshop/app/views.py
+++ workshop/app/views.py
@@ -3,3 +3,34 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def about_us(request):
+    return render(request, 'about_us.html')
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
+
+def mission_available(request):
+    return render(request, 'mission_available.html')
+
+def my_missions(request):
+    return render(request, 'my_missions.html')
+
+def map(request):
+    return render(request, 'map.html')
+
+def settings(request):
+    return render(request, 'settings.html')
+
+def partenaire(request):
+    return render(request, 'partenaire.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def registration(request):
+    return render(request, 'registration.html')
+
+def informations(request):
+    return render(request, 'informations.html')
+
